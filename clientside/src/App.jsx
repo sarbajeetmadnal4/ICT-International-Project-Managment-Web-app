da4f6bc32d9100a735549136ccb854b1be2c0b33
suzzit <bajagainsuzzit@gmail.com>
2023-04-21 15:02:22 +0545

    setup folder structures

--- clientside/src/App.jsx
+++ clientside/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Signup from "./pages/Signup"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>Hello world!</div>
+    <div className="container">
+      <Signup />
+    </div>
   )
 }
 
